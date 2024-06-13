f42862f04f0ae21f67be7080df00ddaa7afff9db
IgaJ <i.jarzewicz@gmail.com>
2024-06-13 13:46:00 +0200

    separate scrape and save operations

--- src/main/java/co/cosmose/scraping_homework/content/PublisherContentScraper.java
+++ src/main/java/co/cosmose/scraping_homework/content/PublisherContentScraper.java
@@ -20,26 +20,38 @@ public class PublisherContentScraper {
     private final PublisherContentRepository publisherContentRepository;
 
     @Scheduled(fixedRate = 3600000)
-    public void scrapeAndSave() {
+    public void scrapeAndSaveScheduled() {
+        List<PublisherContent> contentList = scrape();
+        save(contentList);
+    }
+
+    public List<PublisherContent> scrape() {
+        List<PublisherContent> contents = new ArrayList<>();
         try {
             Document document = Jsoup.connect("https://connect.thairath.co.th/ws/kaikai/content/mirror").get();
             List<Element> items = document.select("item");
-            List<PublisherContent> contents = new ArrayList<>();
             for (Element item : items) {
                 // no NPE validation or Optional, element.select.text() never returns a null
                 PublisherContent content = mapToPublisherContent(item);
+                logIfEmptyContent(content);
                 contents.add(content);
             }
-                for (PublisherContent content : contents) {
-                    if (!publisherContentRepository.existsByArticleUrl(content.getArticleUrl())) {
-                        publisherContentRepository.save(content);
-                        log.debug("Saved new content with articleUrl: {}", content.getArticleUrl());
-                    }
-                }
-            } catch(IOException e){
-                log.error("Error during scraping" + e.getMessage());
+        } catch (IOException e) {
+            log.error("Error during scraping" + e.getMessage());
+        }
+        return contents;
+    }
+
+    public void save(List<PublisherContent> contentList) {
+        for (PublisherContent content : contentList) {
+            if (!publisherContentRepository.existsByArticleUrl(content.getArticleUrl())) {
+                publisherContentRepository.save(content);
+                log.debug("Saved new content with articleUrl: {}", content.getArticleUrl());
+            } else {
+                log.error("Content with articleUrl {} already exists", content.getArticleUrl());
             }
         }
+    }
 
     private PublisherContent mapToPublisherContent(Element element) {
         String articleUrl = element.select("link").text(); // returns empty string if null
@@ -59,6 +71,27 @@ public class PublisherContentScraper {
                 .originalContent(originalContent)
                 .mainImageUrl(mainImageUrl)
                 .build();
+    }
+
+    private void logIfEmptyContent(PublisherContent content) {
+        if (content.getArticleUrl().isEmpty()) {
+            log.debug("Url is missing for content with title: {}", content.getTitle());
+        }
+        if (content.getTitle().isEmpty()) {
+            log.debug("Title is missing for articleUrl: {}", content.getArticleUrl());
+        }
+        if (content.getAuthor().isEmpty()) {
+            log.debug("Author is missing for articleUrl: {}", content.getArticleUrl());
+        }
+        if (content.getHtmlContent().isEmpty()) {
+            log.debug("Html content is missing for articleUrl: {}", content.getArticleUrl());
+        }
+        if (content.getOriginalContent().isEmpty()) {
+            log.debug("Original content is missing for articleUrl: {}", content.getArticleUrl());
+        }
+        if (content.getMainImageUrl().isEmpty()) {
+            log.debug("Image url is missing for articleUrl: {}", content.getArticleUrl());
+        }
     }
 }
 
